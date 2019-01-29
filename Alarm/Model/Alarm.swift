4bdb811bb50c7231f06159cf079d7103e5b0eb11
CameronMilliken <cameron.milliken@gmail.com>
2019-01-29 16:32:22 -0700

    Finished project

---
+++ Alarm/Model/Alarm.swift
@@ -0,0 +1,9 @@
+//
+//  Alarm.swift
+//  Alarm
+//
+//  Created by Cameron Milliken on 1/28/19.
+//  Copyright © 2019 Cameron Milliken. All rights reserved.
+//
+
+import Foundation
---
+++ Alarm/View Controllers/SwitchTableViewCell.swift
@@ -0,0 +1,24 @@
+//
+//  SwitchTableViewCell.swift
+//  Alarm
+//
+//  Created by Cameron Milliken on 1/28/19.
+//  Copyright © 2019 Cameron Milliken. All rights reserved.
+//
+
+import UIKit
+
+class SwitchTableViewCell: UITableViewCell {
+
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+    override func setSelected(_ selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+
+        // Configure the view for the selected state
+    }
+
+}
